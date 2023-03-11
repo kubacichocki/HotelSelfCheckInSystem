98752d03eee98c88bcd7fc759f07ee681913c28a
Jakub Cichocki <cichykanal32@gmail.com>
2023-03-11 13:06:39 +0000

    Moved javascript to a seperate file

---
+++ script.js
@@ -0,0 +1,117 @@
+// Refresh page in order to change the language
+function refreshPage(selectedOption) {
+    window.location.hash = selectedOption;
+    window.location.reload();
+  }
+
+  // Event listeners
+  document.getElementById("spanish").addEventListener("click", function(){ 
+      refreshPage('#es');
+  });
+
+  document.getElementById("english").addEventListener("click", function(){ 
+        refreshPage('#eng');
+    });
+
+    document.getElementById("polish").addEventListener("click", function(){ 
+        refreshPage('#pl');
+    });
+
+    document.getElementById("bulgarian").addEventListener("click", function(){ 
+        refreshPage('#bg');
+    });      
+    document.getElementById("romanian").addEventListener("click", function(){ 
+        refreshPage('#ro');
+      
+    });
+
+
+  //Languages 
+  var languages = {
+      eng: {
+          welcome: "Welcome to our hotel!",
+          instruction: "In order to check in, fill the form below ",
+          name: "Enter your name",
+          confirmationNumber: "Enter you booking confirmation number",
+          warning: "Make sure you enter correct details",
+          submit: "Submit"
+      },
+      es: {
+          welcome: 'Binevenidos todos!',
+          instruction: "Para registrarse, complete el siguiente formulario",
+          name: "Introduzca su nombre",
+          confirmationNumber: "Introduzca su número de confirmación de reserva",
+          warning: "Asegúrese de ingresar los detalles correctos",
+          submit: "Entregar"
+      },
+      pl: {
+        welcome: "Witaj w naszym hotelu!",
+        instruction: "Aby się zameldować wypełnij formularz",
+        name: "Podaj swoje imię",
+        confirmationNumber: "Podaj numer rezerwacji",
+        waring: "Upewnij się, że podane dane są prawidłowe",
+        submit: "Wyslij"
+      },
+
+      bg: {
+          welcome: "Добре дошли във нашия хотел!",
+          instruction: "За да се регистрирате,попълнете формата по-долу! ",
+          name: "Въведете вашето име",
+          confirmationNumber: "Въведете вашия номер на резервация",
+          warning: "Уверете се че въвеждате правилно вашите данни",
+          submit: "Изпрати"
+      },
+       ro: {
+          welcome: "Bine ati venit la hotelul nostru!",
+          instruction: "Pentru a face rezervare, completati formularul de mai jos",
+          name: "Introduceti-va numele complet",
+          confirmationNumber: "Introduceti-va numarul confirmarii rezervarii",
+          warning: "Asigurarti-va ca introduceti datele corecte",
+          submit: "Incarcati"
+       }
+           
+  };
+
+  // Define language through window hash
+  if(window.location.hash) {
+      if(window.location.hash === "#eng"){
+          header1.textContent = languages.eng.welcome
+          header2.textContent = languages.eng.instruction
+          document.getElementById("name").innerHTML = languages.eng.name
+          document.getElementById("confirmationNumber").innerHTML = languages.eng.confirmationNumber
+          document.getElementById("warning").innerHTML = languages.eng.warning
+          document.querySelector('button[type="submit"]').innerHTML = languages.eng.submit
+      }
+      if(window.location.hash === "#es"){
+          header1.textContent = languages.es.welcome
+          header2.textContent = languages.es.instruction
+          document.getElementById("name").innerHTML = languages.es.name
+          document.getElementById("confirmationNumber").innerHTML = languages.es.confirmationNumber
+          document.getElementById("warning").innerHTML = languages.es.warning
+          document.querySelector('button[type="submit"]').innerHTML = languages.es.submit
+      }
+      if(window.location.hash === "#pl"){
+          header1.textContent = languages.pl.welcome
+          header2.textContent = languages.pl.instruction
+          document.getElementById("name").innerHTML = languages.pl.name
+          document.getElementById("confirmationNumber").innerHTML = languages.pl.confirmationNumber
+          document.getElementById("warning").innerHTML = languages.pl.warning
+          document.querySelector('button[type="submit"]').innerHTML = languages.pl.submit
+      }
+      if(window.location.hash === "#bg"){
+          header1.textContent = languages.bg.welcome
+          header2.textContent = languages.bg.instruction
+          document.getElementById("name").innerHTML = languages.bg.name
+          document.getElementById("confirmationNumber").innerHTML = languages.bg.confirmationNumber
+          document.getElementById("warning").innerHTML = languages.bg.warning
+          document.querySelector('button[type="submit"]').innerHTML = languages.bg.submit
+      }
+       if(window.location.hash === "#ro"){
+          header1.textContent = languages.ro.welcome
+          header2.textContent = languages.ro.instruction
+          document.getElementById("name").innerHTML = languages.ro.name
+          document.getElementById("confirmationNumber").innerHTML = languages.ro.confirmationNumber
+          document.getElementById("warning").innerHTML = languages.ro.warning
+          document.querySelector('button[type="submit"]').innerHTML = languages.ro.submit
+       }
+      };
